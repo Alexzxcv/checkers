8ea6f8946187e3dab9e408f356e9512cc9cdd408
Alezxcv <budikov2011@gmail.com>
2022-09-06 02:54:11 +0300

    add: board

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(".row8:odd").append('<img src=img/black1.png>')
+$(".row7:even").append('<img src=img/black1.png>')
+$(".row6:odd").append('<img src=img/black1.png>')
+
+$(".row3:even").append('<img src=img/white1.png>')
+$(".row2:odd").append('<img src=img/white1.png>')
+$(".row1:even").append('<img src=img/white1.png>')
+
