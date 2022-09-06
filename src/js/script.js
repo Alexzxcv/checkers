f67a05850544f281dab0fc438bfc9323fd572a1d
FixIT <alexander.budikov@faradaydc.com>
2022-09-06 19:23:15 +0300

    add: cell

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,13 @@ $(".row3:even").append('<img src=img/white1.png>')
 $(".row2:odd").append('<img src=img/white1.png>')
 $(".row1:even").append('<img src=img/white1.png>')
 
+let cell = $('.container');
+
+
+
+function createCell () {
+    for (let i = 0; i < 64; i++) {
+        cell.append(`<span class=cell id=${i}cell>${i}</span>`);
+    }
+}
+createCell();
